b080fdd716783b6e7a253fc8df08661fcc683a16
Hae-Riri <ehl3288@naver.com>
2020-08-15 18:47:08 +0900

    CRUD - create Test

--- src/main/java/com/example/study/model/entity/User.java
+++ src/main/java/com/example/study/model/entity/User.java
@@ -2,6 +2,7 @@ package com.example.study.model.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,12 +12,9 @@ import java.time.LocalDateTime;
 //DB의 table과 동일해 물론 @Table도 있지만 클래스명과 테이블명이 같으면 자동매칭임
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User {
-
-    public User(){
-
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -1,14 +1,29 @@
 package com.example.study.repository;
 
 import com.example.study.StudyApplicationTests;
+import com.example.study.model.entity.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
 
 public class UserRepositoryTest extends StudyApplicationTests {
     @Autowired
     private UserRepository userRepository;
 
+    @Test
     public void create(){
+        User user = new User();
+        //id는 autoIncrement라 자동으로 넣어지니까 우리가 set하지 않아
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-3333-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("TestUser3");
 
+        //save함수는 User로 반환해, 얜 id도 달고 나오지
+        User newUser = userRepository.save(user);
+        System.out.println("new User : "+newUser);
     }
 
     public void read(){
